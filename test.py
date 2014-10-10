b7754441f8a152fe49be0f1b486169c0fe31d66b
Gilded Honour <me@gildedhonour.com>
2014-10-10 16:41:46 +0800

    refactored1; part implemented

---
+++ test.py
@@ -0,0 +1,26 @@
+from quickbooks2 import QuickBooks
+
+app_token = "eb81179fbbb15b45e4baeb4b665456d40dc6" # not sure what this is for
+consumer_key = "qyprd0mBqvG5ZNPkkchfbZez3eKq0K"
+consumer_secret = "QuwKEiFGHEKubirxTAVkTQkKXEYmiHIEGzMJscUY"
+callback_url = "https://www.somecompany.com/callback"
+access_token = "qyprdXNLmLvvQEUgR1v3E7aaa3oK5LvKth42biHFv41coaan"
+access_token_secret = "fa4slHamwst8lXqeGnNNr0FueuzUxFYxHE1vKBXo"
+company_id = 1243290815  # this is a paid company I set up for this job
+attachment_dir = "for_upload" # for your convenience
+qb = QuickBooks(consumer_key=consumer_key, consumer_secret=consumer_secret, 
+                access_token=access_token, 
+                access_token_secret=access_token_secret, 
+                company_id=company_id,
+                verbosity=10)
+
+existing_attachables = qb.get_objects("Attachable")
+print "Existing Attachable objects length: %d" % len(existing_attachables)
+
+new_attachment1_path = attachment_dir+"/test1.pdf"   
+new_attachment1_id = qb.upload_file(new_attachment1_path)
+
+updated_attachables_list = qb.get_objects("Attachable")
+print "Updated list of Attachable objects length: %d" % len(updated_attachables_list)
+
+# qb.download_file("2800000000001829824")
